563cbb40f6bccf4013fca3151c58daf5990d7f59
shash <shash@RANJIT>
2022-01-20 17:13:19 +0530

    push1 modification

---
+++ com.crm.comcast.sdet28/src/test/java/practiceDemo/GitHub.java
@@ -0,0 +1,11 @@
+package practiceDemo;
+
+import org.testng.annotations.Test;
+
+public class GitHub {
+	@Test
+	public void pushOperation() {
+		System.out.println("Push the code to the database");
+}
+
+}
